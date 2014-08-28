6ffac588a70097ffc9ea98495960297221b54eb6
kapaseker <panoogunker@gmail.com>
2014-08-28 17:39:31 +0800

    建立的算法工程

--- src/com/azalea/common/BinNode.java
+++ src/com/azalea/common/BinNode.java
@@ -1,8 +1,20 @@
 package com.azalea.common;
 
 public class BinNode<T> {
-	Comparable<T> node;
+	private Comparable<T> node;
 
-	BinNode<T> leftNode;
-	BinNode<T> rightNode;
+	private BinNode<T> leftNode;
+	private BinNode<T> rightNode;
+	
+	public Comparable<T> getData(){
+		return node;
+	}
+	
+	public boolean hasLeftNode(){
+		return leftNode!=null;
+	}
+	
+	public boolean hasRightNode(){
+		return rightNode!=null;
+	}
 }
--- src/com/azalea/common/BinTreeGennerater.java
+++ src/com/azalea/common/BinTreeGennerater.java
@@ -3,6 +3,7 @@ package com.azalea.common;
 public class BinTreeGennerater {
 	
 	public static <T> BinNode<T> getBinTree(T[] arrObj){
+		
 		BinNode<T> rootBin = null;
 		if(arrObj != null){
 			
@@ -10,5 +11,7 @@ public class BinTreeGennerater {
 		
 		return rootBin;
 	}
+	
+	
 	
 }
